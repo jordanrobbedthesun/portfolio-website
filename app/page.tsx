d2f91f6153c0c7f8ac31202df6a4e7780a83eba6
jordanrobbedthesun <jordanrobbedthesun@gmail.com>
2025-07-02 17:09:42 -0400

    new tab

--- app/page.tsx
+++ app/page.tsx
@@ -33,6 +33,7 @@ export default function Home() {
             <Link href="#top" className="hover:text-gray-400">Home</Link>
             <Link href="#projects" className="hover:text-gray-400">Projects</Link>
             <a href="../public/JordanRobertsonResume.pdf" target="_blank" className="hover:text-gray-400">Resume</a>
+            <Link href="#soon" className="hover:text-gray-400">soon</Link>
             <Link href="#contact" className="hover:text-gray-400">Contact</Link>
           </div>
         </div>
